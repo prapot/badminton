a2031ce7a89b65529c9a6ac49b12a0e099511109
ku-yart <papot2012@gmail.com>
2026-02-26 18:01:30 +0700

    docker deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   env: {
     NEXT_PUBLIC_STRAPI_BASE_URL:
       process.env.STRAPI_BASE_URL || "http://localhost:1337",
